2a8b23e1b88da90a35abffda0cf399adca920ce5
Abhilash6766 <20b81a1203@cvr.ac.in>
2025-02-01 12:17:30 -0500

    test

--- QuestionService.java
+++ QuestionService.java
@@ -5,7 +5,7 @@ public class QuestionService {
     Question questions[] = new Question[5];
     private String answer[] = new String[5];
     public QuestionService(){
-        questions[0] = new Question(1,"What is my nic name","Abhi","Abhilash","Nani","Reddy","Nani");
+        questions[0] = new Question(1,"What is my nic name","Absdsd","Abhilash","Nani","Reddy","Nani");
         questions[1] = new Question(1,"What is my nic name","Abhi","Abhilash","Nani","Reddy","Nani");
         questions[2] = new Question(1,"What is my nic name","Abhi","Abhilash","Nani","Reddy","Nani");
         questions[3] = new Question(1,"What is my nic name","Abhi","Abhilash","Nani","Reddy","Nani");
